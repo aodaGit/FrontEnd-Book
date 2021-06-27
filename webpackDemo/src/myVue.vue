92b1c538f6fe95da8b950829e8495b9c1bbf9ef7
aoda2826@qq.com <aoda2826@qq.com>
2021-06-28 07:18:51 +0800

    更新

---
+++ webpackDemo/src/myVue.vue
@@ -0,0 +1,18 @@
+<template lang="">
+    <div>
+        测试vue文件
+        <span>{{initdata}}</span>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            initdata:'vue开发环境启动成功'
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ webpackDemo/src/vue.js
@@ -0,0 +1,5 @@
+import Vue from "vue"
+import myVue from "./myVue.vue"
+new Vue({
+    render: h => h(App)
+}).$mount('#app')
